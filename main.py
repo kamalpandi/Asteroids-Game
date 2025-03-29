b45b212cf4ccb6724ca0a74ec2d27ac6d0e87347
kamal <kamalpandi123@gmail.com>
2025-03-29 13:17:06 +0000

    game loop

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+from operator import truediv
 import pygame
+import os
+
+os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
 
 def main():
-    print("Starting Asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return
+        while True:
+            pygame.Surface.fill(screen, (0, 0, 0))
+            pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
