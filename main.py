130a7a1ced928580c614e6ec9a3d4de72bda90eb
kamalpandi <kamalpandi@gmail.com>
2025-12-03 15:40:12 +0530

    screen added

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-
-
+from logger import log_state
 
 if __name__ == "__main__":
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     VERSION = pygame.version.ver
     print("Starting Asteroids with pygame version:", VERSION)
-    print("Screen width:", SCREEN_WIDTH,"\n","Screen height:", SCREEN_HEIGHT)
+    print("Screen width:", SCREEN_WIDTH, "\n", "Screen height:", SCREEN_HEIGHT)
