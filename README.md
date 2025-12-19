2f2b7e0900f732deb24ceedba5db7e401ea86895
kamalpandi <kamalpandi@gmail.com>
2025-12-19 12:00:15 +0530

    CH3-L3

---
+++ README.md
@@ -0,0 +1,10 @@
+# Going to build a simple video game, based on the classic Asteroids. If you've never played before, [you can take a look at this](https://www.echalk.co.uk/amusements/Games/asteroidsClassic/ateroids.html) (slightly different) version of the game
+
+## Learning Goals
+
+The learning goals of this project are as follows:
+
+- To introduce multi-file Python projects  
+- To get used to referencing documentation
+- To show a real-world use case for object-oriented programming  
+- To have a ton of fun building a project!
--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
+    clock_obj = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player_obj = Player(x, y)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     state = True
+
     while state:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                state = False
                 return
 
         pygame.Surface.fill(screen, color=(0, 0, 0))
+        player_obj.draw(screen=screen)
+        dt = clock_obj.tick(60) / 1000
+        player_obj.update(dt)
         pygame.display.flip()
+
     VERSION = pygame.version.ver
     print("Starting Asteroids with pygame version:", VERSION)
     print("Screen width:", SCREEN_WIDTH, "\n", "Screen height:", SCREEN_HEIGHT)
