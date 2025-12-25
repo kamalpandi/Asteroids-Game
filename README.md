8e318f9498733d9f1f564a54c7b43731901805d8
kamalpandi <kamalpandi@gmail.com>
2025-12-25 10:34:48 +0530

    CH4L7

--- README.md
+++ README.md
@@ -10,3 +10,18 @@ The learning goals of this project are as follows:
 - To get used to referencing documentation
 - To show a real-world use case for object-oriented programming  
 - To have a ton of fun building a project!
+
+## Extending ideas
+
+- Add a scoring system
+- Implement multiple lives and respawning
+- Add an explosion effect for the asteroids
+- Add acceleration to the player movement
+- Make the objects wrap around the screen instead of disappearing
+- Add a background image
+- Create different weapon types
+- Make the asteroids lumpy instead of perfectly round
+- Make the ship have a triangular hit box instead of a circular one
+- Add a shield power-up
+- Add a speed power-up
+- Add bombs that can be dropped
--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
+import random
 import pygame
-from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS, LINE_WIDTH
 from circleshape import CircleShape
+from logger import log_event
 
 
 class Asteroid(CircleShape):
@@ -18,3 +20,22 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+
+        log_event("asteroid_split")
+        random_angle = random.uniform(20, 50)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        asteroid_movement_1 = self.velocity.rotate(random_angle)
+        asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_1.velocity = asteroid_movement_1 * 1.2
+
+        asteroid_movement_2 = self.velocity.rotate(-random_angle)
+        asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+        asteroid_2.velocity = asteroid_movement_2 * 1.2
